29642c6d4953c02bf9a5b1e4e2440970af582c78
vivianink1991 <binggan26@126.com>
2022-07-17 11:32:20 +0800

    change header patch

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! change header
+            Lerna is the original monorepo tool! change header patch
         </header>
     );
 }
